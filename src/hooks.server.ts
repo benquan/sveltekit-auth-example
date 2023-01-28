66c9f3afe8fc861077341a8e2ec56ab6e28b80e4
benquan <benquan@gmail.com>
2023-01-28 08:34:59 -0600

    Add prisma and google

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,19 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import Google from '@auth/core/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '$lib/prismadb';
+
+import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
+
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  adapter: PrismaAdapter(prisma),
+
+  providers: [
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
+
+  ],
+
 })
